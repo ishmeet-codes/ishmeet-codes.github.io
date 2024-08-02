c7b1e41ee13937319ba33a35db48c2fec3f56d43
ishmeet-codes <ishmeetsinghxa@gmail.com>
2024-08-03 01:44:00 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'markdown-pwa-cache-v1';
+const resourcesToCache = [
+  '/',
+  '/index.md',
+  '/script.js',
+  '/assets/icons/final-imag.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(resourcesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
